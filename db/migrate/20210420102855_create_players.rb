b7afd8c4d097aafac7e2a9084b519619cfb5bdf7
3ypup <posilay.pochashe@gmail.com>
2021-05-20 15:07:15 +0300

    littlefix

--- db/migrate/20210420102855_create_players.rb
+++ db/migrate/20210420102855_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[5.2]
   def change
     create_table :players do |t|
 
-    	t.integer 	:number
+    	t.integer :number
     	t.string 	:name
     	t.integer	:gold
     	t.integer	:good_quests_done
