3258c01426d5b2051109c4a64f1b04d0925c8790
3ypup <posilay.pochashe@gmail.com>
2021-05-25 12:31:55 +0300

    ready to load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,33 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+   ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
+ #  :tls => true,
+  # :address => "smtp.gmail.com",
+   #:port => 587,
+   #:domain => "gmail.com",
+   #:authentication => :login,
+   #:user_name => Rails.application.credentials.user_name
+   #:password => Rails.application.credentials.email_pass
+ #}
+
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.default_url_options = { :host => '185.188.183.243' }
+ActionMailer::Base.smtp_settings = {
+
+
+  enable_starttls_auto: true,
+  openssl_verify_mode: 'none',
+  #ssl:             true,
+  domain:         'gmail.com',
+  address:        'smtp.gmail.com', # default: localhost
+  port:           '587',                  # default: 25
+  user_name:      Rails.application.credentials.user_name,
+  password:       Rails.application.credentials.email_pass,
+  authentication: :plain              # :plain, :login or :cram_md5
+}
+
 end
